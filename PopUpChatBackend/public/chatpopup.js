0ab03f8cca92babe813445015abb4b1d0dda0e19
MDRanaHardik <hardik.rana@minddeft.net>
2023-06-07 14:28:16 +0530

    Added live visitor with ip

--- PopUpChatBackend/public/chatpopup.js
+++ PopUpChatBackend/public/chatpopup.js
@@ -306,7 +306,7 @@ function RenderChatPopUp(color) {
             </div>
 
             <!-- Chat input and buttons -->
-            <div style="background-color: rgb(219 234 254) !important; !important; padding: 4% !important">
+            <div style="background-color: rgb(219 234 254) !important; padding: 4% !important">
                 <button id="chatnowbtn" onclick="ChatNowBtn()" class="poppins"
                     style="background-color: ${color} !important; color: white !important;  width: 100% !important; padding: 10px 0px 10px 0px !important; font-size: 1rem !important; border-radius: 13px !important; border: none !important;">Chat
                     now</button>
