e19bf312bd62e661ea6448cc6ac9907938931600
MDRanaHardik <hardik.rana@minddeft.net>
2023-06-07 18:12:43 +0530

    Iframe chat widget

--- PopUpChatBackend/public/iframechat/main.js
+++ PopUpChatBackend/public/iframechat/main.js
@@ -17,12 +17,14 @@ async function StartChatIo(id){
 
     await fetch(`${API_HOST}/client/getwidget/${id}`).then((res) => res.json()).then((data) => {
         if (data[0]["Property_status"]) {
+            let colorcode = data[0]["Widget"]["Widget_color"];
+            console.log(data[0]["Widget"]["Widget_color"]);
             document.body.innerHTML +=`
-                <iframe src="https://popupchat.onrender.com/iframechat/chat.html?id=${id}" id="chat" frameborder="0" style="visibility: hidden !important; background: transparent !important; height: 480px !important; width: 310px !important;  z-index:2147483647 !important; position: fixed !important;bottom: 15% !important;right: 2% !important;"></iframe>
+                <iframe src="https://popupchat.onrender.com/iframechat/chat.html?id=${id}" id="chat" frameborder="0" style="visibility: hidden !important; background: transparent !important; height: 480px !important; width: 310px !important;  z-index:2147483647 !important; position: fixed !important;bottom: 13% !important;right: 2% !important;"></iframe>
                 
                 <div class="" style="z-index:2147483647 !important; position: fixed !important;bottom: 4% !important;right: 2% !important;">
                     <button style="z-index:2147483647 !important;padding:0% !important;
-                    background-color: ${data[0]["Widget"]["Widget_color"]} !important;
+                    background-color: ${colorcode} !important;
                     border: 0 !important;
                     border-radius: 100% !important;
                     height: 50px !important;
@@ -45,7 +47,7 @@ async function StartChatIo(id){
                     <button style="
                     z-index:2147483647 !important;
                     padding:0% !important;
-                    background-color: ${data[0]["Widget"]["Widget_color"]} !important;
+                    background-color: ${colorcode} !important;
                     border: 0 !important;
                     border-radius: 100% !important;
                     height: 50px !important;
@@ -70,56 +72,7 @@ async function StartChatIo(id){
         }
     });
 
-    document.body.innerHTML +=`
-    <iframe src="https://popupchat.onrender.com/chat.html?id=${id}" id="chat" frameborder="0" style="visibility: hidden !important; background: transparent !important; height: 480px !important; width: 310px !important;  z-index:2147483647 !important; position: fixed !important;bottom: 15% !important;right: 2% !important;"></iframe>
-    
-    <div class="" style="z-index:2147483647 !important; position: fixed !important;bottom: 4% !important;right: 2% !important;">
-        <button style="z-index:2147483647 !important;padding:0% !important;
-          background-color: blue !important;
-          border: 0 !important;
-          border-radius: 100% !important;
-          height: 50px !important;
-          width: 50px !important;
-          float: right !important;
-          margin-top: 5% !important; 
-          box-shadow: 0 3px 10px rgb(0 0 0 / 0.2) !important;
-          background-position: center !important;
-          background-size: 30px !important;
-          background-repeat: no-repeat !important;
-          background-image: url('https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/512/FFFFFF/external-chat-social-media-ui-tanah-basah-glyph-tanah-basah.png') !important;
-        animation: bounce 1s infinite !important;
-        "
-        id="chatio" onclick="ChatIo()"
-        >
-            
-        </button>
-
-
-        <button style="
-          z-index:2147483647 !important;
-          padding:0% !important;
-          background-color: blue !important;
-          border: 0 !important;
-          border-radius: 100% !important;
-          height: 50px !important;
-          width: 50px !important;
-          float: right !important;
-          margin-top: 5% !important;
-          box-shadow: 0 3px 10px rgb(0 0 0 / 0.2) !important;
-          display: none !important;
-          background-position: center !important;
-          background-size: 30px !important;
-          background-repeat: no-repeat !important;
-          background-image: url('https://img.icons8.com/ios-glyphs/512/FFFFFF/delete-sign.png') !important;
-          
-        "
-        id="close" onclick="CloseBtn()"
-        >
-            
-        </button>
-    </div>
-    
-    `
+   
 
 }
 
