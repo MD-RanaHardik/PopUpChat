d13d4f6dbe5b4d08d0022256ff7e0e4849ed2f63
MDRanaHardik <hardik.rana@minddeft.net>
2023-06-07 17:30:38 +0530

    Added live visitor with ip

--- PopUpChatBackend/public/iframechat/main.js
+++ PopUpChatBackend/public/iframechat/main.js
@@ -9,7 +9,66 @@ if (document.currentScript.getAttribute('id') != null) {
 
 
 
-function StartChatIo(id){
+async function StartChatIo(id){
+
+    let API_HOST = "https://popupchat.onrender.com";
+
+    // let API_HOST = "http://127.0.0.1:4000";
+
+    await fetch(`${API_HOST}/client/getwidget/${property_ID}`).then((res) => res.json()).then((data) => {
+        if (data[0]["Property_status"]) {
+            document.body.innerHTML +=`
+                <iframe src="https://popupchat.onrender.com/iframechat/chat.html?id=${id}" id="chat" frameborder="0" style="visibility: hidden !important; background: transparent !important; height: 480px !important; width: 310px !important;  z-index:2147483647 !important; position: fixed !important;bottom: 15% !important;right: 2% !important;"></iframe>
+                
+                <div class="" style="z-index:2147483647 !important; position: fixed !important;bottom: 4% !important;right: 2% !important;">
+                    <button style="z-index:2147483647 !important;padding:0% !important;
+                    background-color: ${data[0]["Widget"]["Widget_color"]} !important;
+                    border: 0 !important;
+                    border-radius: 100% !important;
+                    height: 50px !important;
+                    width: 50px !important;
+                    float: right !important;
+                    margin-top: 5% !important; 
+                    box-shadow: 0 3px 10px rgb(0 0 0 / 0.2) !important;
+                    background-position: center !important;
+                    background-size: 30px !important;
+                    background-repeat: no-repeat !important;
+                    background-image: url('https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/512/FFFFFF/external-chat-social-media-ui-tanah-basah-glyph-tanah-basah.png') !important;
+                    animation: bounce 1s infinite !important;
+                    "
+                    id="chatio" onclick="ChatIo()"
+                    >
+                        
+                    </button>
+
+
+                    <button style="
+                    z-index:2147483647 !important;
+                    padding:0% !important;
+                    background-color: ${data[0]["Widget"]["Widget_color"]} !important;
+                    border: 0 !important;
+                    border-radius: 100% !important;
+                    height: 50px !important;
+                    width: 50px !important;
+                    float: right !important;
+                    margin-top: 5% !important;
+                    box-shadow: 0 3px 10px rgb(0 0 0 / 0.2) !important;
+                    display: none !important;
+                    background-position: center !important;
+                    background-size: 30px !important;
+                    background-repeat: no-repeat !important;
+                    background-image: url('https://img.icons8.com/ios-glyphs/512/FFFFFF/delete-sign.png') !important;
+                    
+                    "
+                    id="close" onclick="CloseBtn()"
+                    >
+                        
+                    </button>
+                </div>
+                
+                `
+        }
+    });
 
     document.body.innerHTML +=`
     <iframe src="https://popupchat.onrender.com/chat.html?id=${id}" id="chat" frameborder="0" style="visibility: hidden !important; background: transparent !important; height: 480px !important; width: 310px !important;  z-index:2147483647 !important; position: fixed !important;bottom: 15% !important;right: 2% !important;"></iframe>
